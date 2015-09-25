db4cb12535dab5ceb688f59d35d8606118fd1565
Hai Tang <htang16@jhu.edu>
2015-09-25 13:57:00 -0400

    StackWithMin

--- src/Ch3/StacksAndQueues/Ch3_2_Stackmin.java
+++ src/Ch3/StacksAndQueues/Ch3_2_Stackmin.java
@@ -1,38 +1,95 @@
 package Ch3.StacksAndQueues;
 
-public class Ch3_2_Stackmin {
-	Node top;
-	Object min;
-	
-	Object pop(){
-		if(top!=null){
-			Object item = top.data;
-			top = top.next;
-			return item;
-		}
-		return null;
+import java.util.Stack;
+
+public class Ch3_2_Stackmin extends Stack<Integer>{//extends java.util.Stack
+	Stack<Integer> s; //store the min values
+	public Ch3_2_Stackmin(){
+		s = new Stack<Integer>();
 	}
 	
-	void push(Object item){
-		if(min == null){
-			min = item;
+	public int min(){
+		if(s.isEmpty()){
+			return Integer.MAX_VALUE;
 		}
 		else{
-			if((int)item<(int)min){
-				min = item;
-			}
-		}	
-		
-		Node t = new Node(item);
-		t.next = top;
-		top = t;
+			return s.peek();//take out the real min from the extra stack s
+		}
 	}
 	
-	Object peek(){
-		return top.data;
+	public void push(int value){
+		if(value <= min()){
+			s.push(value); //push the new min value if necessary
+		}
+		super.push(value);
 	}
 	
-	Object min(){
-		return min;
+	public Integer pop(){
+		int value = super.pop();
+		if(value==min()){
+			s.pop();//get rid of the previous min value from s as well
+		}
+		return value;
+	}
+	
+	public static void main(String[] args){
+		Ch3_2_Stackmin myMinStack = new Ch3_2_Stackmin();
+		myMinStack.push(5);
+		System.out.print(myMinStack.min());
+		System.out.print('\n');
+		myMinStack.push(6);
+		System.out.print(myMinStack.min());
+		System.out.print('\n');
+		myMinStack.push(3);
+		System.out.print(myMinStack.min());
+		System.out.print('\n');
+		myMinStack.push(7);
+		System.out.print(myMinStack.min());
+		System.out.print('\n');
+		myMinStack.pop();
+		System.out.print(myMinStack.min());
+		System.out.print('\n');
+		myMinStack.pop();
+		System.out.print(myMinStack.min());
+		System.out.print('\n');
 	}
 }
+
+
+
+//public class Ch3_2_Stackmin { //没考虑Pop()之后的min处理
+//	Node top;
+//	Object min;
+//	
+//	Object pop(){
+//		if(top!=null){
+//			Object item = top.data;
+//			top = top.next;
+//			return item;
+//		}
+//		return null;
+//	}
+//	
+//	void push(Object item){
+//		if(min == null){
+//			min = item;
+//		}
+//		else{
+//			if((int)item<(int)min){
+//				min = item;
+//			}
+//		}	
+//		
+//		Node t = new Node(item);
+//		t.next = top;
+//		top = t;
+//	}
+//	
+//	Object peek(){
+//		return top.data;
+//	}
+//	
+//	Object min(){
+//		return min;
+//	}
+//}
