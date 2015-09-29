79b6e5b5a988f6e3ba1b9d216b2f09678c61ceda
Hai Tang <htang16@jhu.edu>
2015-09-29 13:43:15 -0400

    CheckBST

---
+++ src/Ch4/TreesAndGraphs/Ch4_5_CheckBST.java
@@ -0,0 +1,20 @@
+package Ch4.TreesAndGraphs;
+
+public class Ch4_5_CheckBST {
+	public boolean chenckBST(TreeNode root){
+		return checkBST(root, null, null);
+	}
+	
+	public boolean checkBST(TreeNode n, Integer min, Integer max){
+		if(n == null){
+			return true;
+		}
+		if( (min!=null && (int) n.data <= min) || (max!=null && (int) n.data>max) ){//check current n
+			return false;
+		}
+		if( (!checkBST(n.left, min, (int) n.data)) || (!checkBST(n.right, (int) n.data, max)) ){ //有false就返回false
+			return false;
+		}
+		return true;
+	}
+}
